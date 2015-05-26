e05f12a6a9439418f5e20c979df87fbf2e6350b9
Daniel Rodriguez <daniel.rodriguez.d@gmail.com>
2015-05-26 19:31:50 +0200

    admin module

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new AgendaBundle\AgendaBundle(),
+            new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
